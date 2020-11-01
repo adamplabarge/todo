c90baa94ba2b41e89c2fcc90c9fa8520279bf9f4
adamplabarge <adamplabarge@gmail.com>
2020-11-01 10:06:03 -0800

    more fullscreen try

--- app/client/src/components/FullScreen/useFullscreenStatus.jsx
+++ app/client/src/components/FullScreen/useFullscreenStatus.jsx
@@ -7,12 +7,9 @@ const useFullscreenStatus = (elRef) => {
 
   const setFullscreen = () => {
     if (elRef.current == null) return
-
-    const requestFullScreen = elRef.requestFullscreen || elRef.mozRequestFullScreen || elRef.webkitRequestFullScreen || elRef.msRequestFullscreen
-    // const cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen
     
-    requestFullScreen
-      .call(requestFullScreen)
+    elRef.current
+      .requestFullscreen()
       .then(() => {
         setIsFullscreen(document[getBrowserFullscreenElementProp()] != null)
       })
